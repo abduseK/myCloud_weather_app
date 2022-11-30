a77ed93fc4dc2eb98c7fa4bd5d4a33580b1513a4
abdelselam kemal <abdelselamkemal74@gmail.com>
2022-11-29 18:54:23 -0800

    further updates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const path = require('path')
 
+// console.log(path.join(__dirname, '../public'))
+
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
-app.use(express.static(publicDirectoryPath))
-// app.com
-// app.com/help
-// app.com/about
+const publicDirectoryPath = path.join(__dirname, '../views')
+
+app.set('view engine', 'hbs')
+app.set('views', publicDirectoryPath)
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'HomePage',
+        text: 'This will be the paragraph of the page'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/help', (req, res) => {
+    res.render('help')
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
